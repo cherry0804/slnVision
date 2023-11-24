6b615bb77729fdd68beb20034b3880726d769c01
Jacky Jie <jakcy2222@gmail.com>
2023-11-24 13:46:43 +0800

    加入專案檔案。

---
+++ prjVision/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjVision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
